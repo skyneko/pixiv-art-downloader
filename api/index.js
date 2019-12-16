ca759b08dea8a8894275a1c63cd65f2bde61874b
SkyNeko <banhgao.nya@gmail.com>
2019-12-16 13:13:16 +0700

    fix bug

--- api/index.js
+++ api/index.js
@@ -1,12 +1,14 @@
 const request = require("request-promise");
+const axios = require("axios")
+
 const cheerio = require("cheerio");
 const fs = require("fs");
 const qs = require('qs');
 const crypto = require("crypto");
-const axios = require("axios")
 
-if (!fs.existsSync("./file"))
-	fs.mkdirSync("./file");
+
+if (!fs.existsSync("./images"))
+	fs.mkdirSync("./images");
 
 const now_time = new Date();
 
@@ -31,11 +33,17 @@ module.exports = {
 	download: function (uri) {
 		function getUrl(uri) {
 			return new Promise((resolve) => {
-				let id = uri.slice(uri.indexOf("illust_id=") + 10, uri.length);
+				let id = uri.slice(uri.indexOf("artworks/") + 9, uri.length);
 				request({
 					headers: {
-						Cookie: "PHPSESSID=20496492_520b5dfd42e6fcc7768ad74a91a60bc9; p_ab_id=2; p_ab_id_2=4; yuid=cwYFh5A55; tag_view_ranking=0xsDLqCEW6~RTJMXD26Ak~GX5cZxE2GY~b_G3UDfpN0~zIv0cf5VVk~BU9SQkS-zU~yZf1XmIy-U~iFcW6hPGPU~EGefOqA6KB~Lt-oEicbBr~zyKU3Q5L4C~nriWjM9urd~y8GNntYHsi~KN7uxuR89w~azESOjmQSV~qvqXJkzT2e~NpsIVvS-GF~Fq4K_8PGib~Ie2c51_4Sp~pzzjRSV6ZO~qiO14cZMBI~qtVr8SCFs5~tgP8r-gOe_~Bd2L9ZBE8q~q303ip6Ui5~RcahSSzeRf~xjfPXTyrpQ~FH69TLSzdM~81BOcT1ZAV~y68AFldGp7~65aiw_5Y72~CLR9k9dHAQ~Is0SiXyaWb~udkRh_mjvd~xZ6jtQjaj9~X_1kwTzaXt~YRDwjaiLZn~1HSjrqSB3U~D8cfQCDYAl~e4Va3SNH5h~SJK3YcGD-h~K0LWjp4M-c~VN7cgWyMmg~x_jB0UM4fe~dehCwztzpj~AVEc3LeUs5~IQ7tgDp_ul~fryDCikS2a~nRp2ZLPLbj~pYlUxeIoeg~j3leh4reoN~1F9SMtTyiX~XEuS3TPyCa~gooMLQqB9a~kym2Z4vnZu~JO16HzBgpd~_hSAdpN9rx~BuxH2AvtZ9~XpYOJt3r5W~Ce-EdaHA-3~-L-4bBqjrT~rezgCfkPbs~4OtoweblrI~gVfGX_rH_Y~AaBbSF1H2D~f-c_0dUV8c~aKAp3RlsBg~CrFcrMFJzz~-StjcwdYwv~jH0uD88V6F~Ltq1hgLZe3~LVSDGaCAdn~pnCQRVigpy~RVRPe90CVr~wyZOlBKxtg~uusOs0ipBx~qnO24qbca3~ANkTxNMKP2~CbmkqBJulM~q3eUobDMJW~tTvZK72fmv~EUwzYuPRbU~89uzPVUy9U~wsim4__rTI~KhVXu5CuKx~HZCnKGc7Gi~X1ZSbphPDx~kGYw4gQ11Z~qWFESUmfEs~ay54Q_G6oX~9TPuxVVpm_~5RvyKm3yea~juumLY9DfB~a-IhwNF_3B~WY10GFG4q3~OVcez6CxIO~HrwdVe4iWc~agX61qa9EJ~iOH7DNLGY6~ZeF-yPWEA5; __utma=235335808.224054593.1525685806.1525685806.1525685833.2; __utmz=235335808.1525685833.2.2.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmv=235335808.|2=login%20ever=yes=1^3=plan=normal=1^5=gender=male=1^6=user_id=20496492=1^9=p_ab_id=2=1^10=p_ab_id_2=4=1^11=lang=en=1; _ga=GA1.2.224054593.1525685806; c_type=18; a_type=0; b_type=1; module_orders_mypage=%5B%7B%22name%22%3A%22sketch_live%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22tag_follow%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22recommended_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22showcase%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22everyone_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22following_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22mypixiv_new_illusts%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22fanbox%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22featured_tags%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22contests%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22user_events%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22sensei_courses%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22spotlight%22%2C%22visible%22%3Atrue%7D%2C%7B%22name%22%3A%22booth_follow_items%22%2C%22visible%22%3Atrue%7D%5D; login_ever=yes; first_visit_datetime_pc=2018-05-28+16%3A24%3A47; privacy_policy_agreement=1; is_sensei_service_user=1",
-						'Content-Type': 'application/x-www-form-urlencoded'
+						'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0',
+						'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+						'Accept-Language': 'en-US,en;q=0.5',
+						'Connection': 'keep-alive',
+						'Upgrade-Insecure-Requests': '1',
+						'Pragma': 'no-cache',
+						'Cache-Control': 'no-cache',
+						'TE': 'Trailers',
 					},
 					uri: "https://www.pixiv.net/ajax/illust/" + id,
 					json: true
@@ -55,8 +63,8 @@ module.exports = {
 				Promise.all(uri.map(el => load(el.img, el.id, this.download)))
 			}
 			else {
-				Promise.all(uri.map(getUrl)).then((res) => { res.map((el) => { load(el.img, el.id, this.download) }) });
-			}
+				Promise.all(uri.map(getUrl)).then((res) => { res.map((el) => { load(el.img, el.id, this.download) }) })
+			} 
 		}
 		function load(url, id, func) {
 			if (url !== undefined) {
@@ -67,7 +75,7 @@ module.exports = {
 						},
 						uri: url
 					})
-						.pipe(fs.createWriteStream(__dirname + "/file/" + id + ".png"))
+						.pipe(fs.createWriteStream("./images/" + id + url.slice(url.length-4,url.length)))
 						.on("close", function () {
 							console.log("save " + id + "." + url.slice(url.length - 3, url.length));
 						});
@@ -94,13 +102,15 @@ module.exports = {
 				}).then(json => { //232
 					if (json != undefined)
 						json.illusts.forEach(el => {
-							illust_arr.push({
-								img: el.meta_single_page.original_image_url,
-								id: el.id
-							});
-							resolve((json.illusts != []) ? illust_arr : "limited");
+							if (el.meta_single_page.original_image_url !== undefined)
+								illust_arr.push({
+									img: el.meta_single_page.original_image_url,
+									id: el.id
+								});
+							console.log(illust_arr);	
+							resolve((json.illusts != []) ? illust_arr : []);
 						})
-					else resolve("limited");
+					else resolve([]);
 				});
 			});
 		});
@@ -110,7 +120,7 @@ module.exports = {
 			this.download(res);
 			return res
 		}).then(res => {
-			if (res != "limited") {
+			if (res != []) {
 				this.downloadAll(userID, opt, n + 30);
 			}
 		});
@@ -197,6 +207,7 @@ module.exports = {
 			}
 		});
 	},
+
 	getAuthAccessToken: function ({ username, password }, callback = () => { }) {
 		
 		axios({
@@ -219,7 +230,7 @@ module.exports = {
 			}),
 			method: "POST"
 		}).then(({data}) => {
-			callback(data.response);
+			callback("Bearer "+data.response.access_token);
 		});
 	}
 } 
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const pixiv = require("./api");
 const fs = require("fs");
-const user = JSON.parse(fs.readFileSync("./account/config.json","utf-8"));
+const user = JSON.parse(fs.readFileSync("./account/config.json", "utf-8"));
 
-
-pixiv.getAuthAccessToken({username: user.username, password: user.password},console.log);
+pixiv.getUserWork(1655331, user,1).then(data => {
+    let img = data.map(val => "https://www.pixiv.net/en/artworks/" + val.id);
+    pixiv.download(img);
+    // console.log(img);
+});
